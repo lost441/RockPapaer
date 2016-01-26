0845d9313dbc8cc41a05af28fa15f3535deb9362
LindileB <bangose.lindile@gmail.co.za>
2016-01-26 12:30:57 +0200

    Bind UI to View-model

--- RockPaper/RockPaper.Wpf/ViewModels/GameViewModel.cs
+++ RockPaper/RockPaper.Wpf/ViewModels/GameViewModel.cs
@@ -2,16 +2,43 @@
 namespace RockPaper.Wpf.ViewModels
 {
     using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using RockPaper.Wpf.Common;
     using RockPaper.Wpf.Models;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Game view model class.
     /// </summary>
-    public class GameViewModel
+    public class GameViewModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// The team name to register
+        /// </summary>
+        private string teamNameToRegister;
+
+        /// <summary>
+        /// The registered team name
+        /// </summary>
+        private string registeredTeamName;
+
+        /// <summary>
+        /// The is active
+        /// </summary>
+        private bool isActive;
+
+        /// <summary>
+        /// The is registered
+        /// </summary>
+        private bool isRegistered;
+
+        /// <summary>
+        /// The is automatic refresh
+        /// </summary>
+        private bool isAutoRefresh;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="GameViewModel"/> class.
         /// </summary>
@@ -30,6 +57,65 @@ namespace RockPaper.Wpf.ViewModels
             this.RefreshGameResultsCommand = new Command(() => true, this.GetGameResults);
             this.PlayHandCommand = new Command(() => true, this.PlayHand);
             this.CheckTurnCommand = new Command(() => true, this.CheckTurn);
+        }
+
+        /// <summary>
+        /// Gets or sets the team name to register.
+        /// </summary>
+        /// <value>
+        /// The team name to register.
+        /// </value>
+        public string TeamNameToRegister
+        {
+            get { return this.teamNameToRegister; }
+            set { this.teamNameToRegister = value; OnPropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the registered team.
+        /// </summary>
+        /// <value>
+        /// The name of the registered team.
+        /// </value>
+        public string RegisteredTeamName
+        {
+            get { return this.registeredTeamName; }
+            set { this.registeredTeamName = value; OnPropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is active.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is active; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsActive
+        {
+            get { return this.isActive; }
+            set { this.isActive = value; OnPropertyChanged(); }
+        }
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is registered.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is registered; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsRegistered
+        {
+            get { return this.isRegistered; }
+            set { this.isRegistered = value; OnPropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is automatic refresh.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is automatic refresh; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsAutoRefresh
+        {
+            get { return this.isAutoRefresh; }
+            set { this.isAutoRefresh = value; OnPropertyChanged(); }
         }
 
         /// <summary>
@@ -79,7 +165,12 @@ namespace RockPaper.Wpf.ViewModels
         /// The check turn command.
         /// </value>
         public Command CheckTurnCommand { get; private set; }
-        
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// Registers the team.
         /// </summary>
@@ -119,6 +210,17 @@ namespace RockPaper.Wpf.ViewModels
         private void CheckTurn()
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Called when [property changed].
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
