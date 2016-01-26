6429452558ca4dd93a3ae885475080195ff3436c
lost441 <lost441@gmail.com>
2016-01-26 15:10:18 +0200

    WCF Testing - Added web.config

---
+++ RockPaper/RockPaper.Tests/WCF Testing/WCFTesting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject.WCF_Testing
+{
+    [TestClass]
+    public class WCFTesting
+    {
+        [TestMethod]
+        public void RegisterTeamWCF()
+        {
+            var client = new RockPaperDEV.RockPaperServiceClient();
+            var team = client.GetTeamByTeamName("Test");
+
+            if (team.Data == null)
+            {
+
+            }
+        }
+    }
+}
