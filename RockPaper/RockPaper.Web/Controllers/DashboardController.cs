f1f04079a2fa17b42e4e6856a1563670b9234761
Mpho Majenge <mshauny@gmail.com>
2016-01-27 10:00:19 +0200

    Revert "Added Dashboard Models and Interfaces"
    This reverts commit 31b3d53efdb15f71eda8d91828ccbe24fc2693fb.

--- RockPaper/RockPaper.Web/Controllers/DashboardController.cs
+++ RockPaper/RockPaper.Web/Controllers/DashboardController.cs
@@ -3,22 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using RockPaper.Web.Contracts;
-using RockPaper.Web.Models;
 
 namespace RockPaper.Web.Controllers
 {
-    public class DashboardController : Controller, IDashboard
+    public class DashboardController : Controller
     {
-
+        // GET: Dashboard
         public ActionResult Index()
         {
-            throw new NotImplementedException();
-        }
-
-        public JsonResult GetResults(int offset = 0, int records = 10, Models.FilterOption filter = FilterOption.None)
-        {
-            
+            return View();
         }
     }
 }
