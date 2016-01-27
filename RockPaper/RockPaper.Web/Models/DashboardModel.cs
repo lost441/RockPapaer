31b3d53efdb15f71eda8d91828ccbe24fc2693fb
Mpho Majenge <mshauny@gmail.com>
2016-01-27 09:41:11 +0200

    Added Dashboard Models and Interfaces
    Added Dashboard Models and Interfaces

---
+++ RockPaper/RockPaper.Web/Models/DashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaper.Web.Models
+{
+    public enum FilterOption
+    {
+        None = 1,
+        Team = 2
+    }
+}
